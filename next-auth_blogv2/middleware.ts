4ca014ca012db47dbe280f1fd33f6caf2444f602
Furt_Tech <none@none.com>
2024-02-29 02:14:46 -0600

    fixed login bug

--- next-auth_blogv2/middleware.ts
+++ next-auth_blogv2/middleware.ts
@@ -6,6 +6,6 @@ export const { auth: middleware } = NextAuth(authConfig);
 
 export const config ={
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher:'/((?!api|_next/static|_next/image|favicon.ico|login|register|$).*)',
+  matcher:'/((?!api|_next/static|_next/image|favicon.ico|register|$).*)',
   
 }
